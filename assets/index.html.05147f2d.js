a7a20fd76dba334a377d9e975ad82c26c72f9f9e
Sebastien Crevier <sebastien.crevier@hotmail.com>
2022-09-14 15:00:22 -0400

    update

--- assets/index.html.05147f2d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as c}from"./app.ef002fa1.js";const n={},_=c("p",null,"Hola",-1),s=[_];function a(l,r){return t(),o("div",null,s)}const i=e(n,[["render",a],["__file","index.html.vue"]]);export{i as default};
